94b41fe84704fbe8e5a13e41565433fc26e463d2
FireLion137 <pietroespo8@gmail.com>
2024-01-26 04:09:38 +0100

    Fix test. Creato application.properties per i test. Aggiornata dependency Selenium. Rimossa dependency junit4. Creata variabile testMode in AccountController. Adesso il login automatico non viene eseguito se in test. Fix TF1Test and checkstyle. Ported TC11Test to JUnit5 and removed useless code. Added checks in TC11Test and delete when user is created.

--- src/test/java/com/is/findyourplace/Unit/TF1Test.java
+++ src/test/java/com/is/findyourplace/Unit/TF1Test.java
@@ -1,5 +1,4 @@
 package com.is.findyourplace.Unit;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import com.is.findyourplace.FindYourPlaceApplication;
 import com.is.findyourplace.persistence.dto.UtenteDto;
@@ -10,40 +9,30 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = FindYourPlaceApplication.class )
-@AutoConfigureMockMvc(addFilters = false)
-public class TF1 {
+@AutoConfigureMockMvc()
+public class TF1Test {
+    @MockBean
+    private AccountService accountService;
+
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private AccountService accountService;
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Before()
-    public void setup()
-    {
-        //Init MockMvc Object and build
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
     @Test
     public void testRegistration() throws Exception {
-        // Mock input data
         UtenteDto utenteDto = new UtenteDto();
         utenteDto.setNome("testnome");
         utenteDto.setCognome("testcognome");
-        utenteDto.setDataNascita(LocalDate.of(1998,03,02));
+        utenteDto.setDataNascita(LocalDate.of(1998, 3,2));
         utenteDto.setUsername("testuser");
         utenteDto.setEmail("test@example.com");
         utenteDto.setPassword("Testpassword1!");
@@ -52,10 +41,9 @@ public class TF1 {
         Mockito.when(accountService.existsByEmail("test@example.com")).thenReturn(false);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
                         .flashAttr("utenteR", utenteDto))
                 .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
-
-        // Additional assertions can be added as needed}
     }
 }
